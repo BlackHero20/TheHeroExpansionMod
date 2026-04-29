32f4856634992e6029cefe8d730e2f6d02181be5
BlackHero20 <guyshzu@gmail.com>
2026-04-29 21:10:33 +0300

    feat: added Vakuu's Prank Card

---
+++ TheHeroExpansionCode/Cards/VakuuCards/VakuusPrank.cs
@@ -0,0 +1,57 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Nodes.Vfx;
+using TheHeroExpansion.TheHeroExpansionCode.Cards;
+using TheHeroExpansion.TheHeroExpansionCode.Powers.VakuuPowers;
+
+namespace TheHeroExpansion.TheHeroExpansionCode.Cards.VakuuCards;
+
+[Pool(typeof(TokenCardPool))]
+public class VakuusPrank() : TheHeroExpansionCard(0,
+    CardType.Power, CardRarity.Ancient,
+    TargetType.Self)
+{
+    
+    protected override IEnumerable<DynamicVar> CanonicalVars => 
+    [
+        new PowerVar<VakuusPrankPower>(15M),
+    ];
+    
+    public override bool ShouldPlay(CardModel card, AutoPlayType autoPlayType)
+    {
+        if (card.Owner != this.Owner) return true;
+        CardPile pile = this.Pile;
+        return (pile != null ? (pile.Type != PileType.Hand ? 1 : 0) : 1) != 0
+               || card is VakuusPrank
+               || autoPlayType != AutoPlayType.None;
+    }
+    
+    public override async Task BeforeTurnEnd(PlayerChoiceContext choiceContext, CombatSide side)
+    {
+        VakuusPrank vakuusPrank = this;
+        TalkCmd.Play(new LocString("cards", "THEHEROEXPANSION-VAKUUS_PRANK.approval"), vakuusPrank.Owner.Creature, VfxColor.Purple);
+        if (vakuusPrank.Owner.Creature.Side != side)
+            return;
+        if (vakuusPrank.Pile?.Type != PileType.Hand)
+            return;
+        await CardCmd.AutoPlay(choiceContext, vakuusPrank, null, AutoPlayType.Default);
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        VakuusPrank vakuusPrank = this;
+        await PowerCmd.Apply<VakuusPrankPower>(choiceContext, vakuusPrank.Owner.Creature, vakuusPrank.DynamicVars["VakuusPrankPower"].BaseValue, vakuusPrank.Owner.Creature, this);
+    }
+    
+    protected override void OnUpgrade()
+    {
+        this.DynamicVars["VakuusPrankPower"].UpgradeValueBy(10M);
+    }
+}
---
+++ TheHeroExpansionCode/Powers/VakuuPowers/VakuusPrankPower.cs
@@ -0,0 +1,24 @@
+﻿using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace TheHeroExpansion.TheHeroExpansionCode.Powers.VakuuPowers;
+public class VakuusPrankPower : TheHeroExpansionPower
+{
+    public override PowerType Type => PowerType.Debuff;
+    public override PowerStackType StackType => PowerStackType.Counter;
+
+    public override async Task AfterTurnEnd(PlayerChoiceContext choiceContext, CombatSide side)
+    {
+        VakuusPrankPower vakuusPrankPower = this;
+        if (vakuusPrankPower.Owner.Player.Creature.Side != side)
+            return;
+        vakuusPrankPower.Flash();
+        await CreatureCmd.Damage(choiceContext, vakuusPrankPower.Owner, (Decimal) vakuusPrankPower.Amount, ValueProp.Unpowered, vakuusPrankPower.Owner, (CardModel) null);
+        VfxCmd.PlayOnCreatureCenter(vakuusPrankPower.Owner, "vfx/vfx_attack_blunt");
+        await PowerCmd.Remove((PowerModel) vakuusPrankPower);
+    }
+}
