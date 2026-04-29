47499fb15d19bf764e503c08a7b0a2c9f3178696
BlackHero20 <guyshzu@gmail.com>
2026-04-29 14:46:42 +0300

    fix: made all ancient relics show up in the compendium

--- TheHeroExpansionCode/Patches/PaelPatch.cs
+++ TheHeroExpansionCode/Patches/PaelPatch.cs
@@ -11,18 +11,15 @@ public static class PaelPatch
 {
     public static void Postfix(Pael __instance, ref IEnumerable<EventOption> __result)
     {
-        if (__instance.Owner == null)
-            return;
-
-        var relic = ModelDb.Relic<PaelsVirus>().ToMutable(); //change this to whatever relic I want
-        relic.Owner = __instance.Owner;
-
-        // Use reflection to call the protected RelicOption method
+        var relic = ModelDb.Relic<PaelsVirus>().ToMutable();
+        
         var method = typeof(AncientEventModel).GetMethod("RelicOption",
             System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance,
             null, new[] { typeof(RelicModel), typeof(string), typeof(string) }, null);
 
         var option = (EventOption)method.Invoke(__instance, new object[] { relic, "INITIAL", null });
-        __result = __result.Append(option);
+        var list = __result.ToList();
+        list.Add(option);
+        __result = list;
     }
 }
--- TheHeroExpansionCode/Patches/TanxPatch.cs
+++ TheHeroExpansionCode/Patches/TanxPatch.cs
@@ -11,18 +11,15 @@ public static class TanxPatch
 {
     public static void Postfix(Tanx __instance, ref IEnumerable<EventOption> __result)
     {
-        if (__instance.Owner == null)
-            return;
-
         var relic = ModelDb.Relic<TestRelic>().ToMutable(); //change this to whatever relic I want
-        relic.Owner = __instance.Owner;
-
-        // Use reflection to call the protected RelicOption method
+        
         var method = typeof(AncientEventModel).GetMethod("RelicOption",
             System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance,
             null, new[] { typeof(RelicModel), typeof(string), typeof(string) }, null);
 
         var option = (EventOption)method.Invoke(__instance, new object[] { relic, "INITIAL", null });
-        __result = __result.Append(option);
+        var list = __result.ToList();
+        list.Add(option);
+        __result = list;
     }
 }
