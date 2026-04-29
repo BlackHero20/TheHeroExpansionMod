a7116b50f5794ea56c3d3d00220d433d14a5cd99
BlackHero20 <guyshzu@gmail.com>
2026-04-29 16:16:52 +0300

    feat: added golden ratio enchantment and steel ball relic functionality

---
+++ TheHeroExpansionCode/Enchantments/GoldenRatio.cs
@@ -0,0 +1,54 @@
+﻿using BaseLib.Abstracts;
+using BaseLib.Extensions;
+using Godot;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using TheHeroExpansion.TheHeroExpansionCode.Extensions;
+
+namespace TheHeroExpansion.TheHeroExpansionCode.Enchantments;
+
+public sealed class GoldenRatio : TheHeroExpansionEnchantment
+{
+    public override bool IsStackable => false;
+
+    protected override string? CustomIconPath
+    {
+        get
+        {
+            var path = $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".EnchantmentImagePath();
+            return ResourceLoader.Exists(path) ? path : null;
+        }
+    }
+    
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new EnergyVar(1) //exists for localization only
+    ];
+
+    public override async Task BeforeTurnEnd(PlayerChoiceContext choiceContext, CombatSide side)
+    {
+        GoldenRatio enchantment = this;
+        
+        if (enchantment.Card == null)
+            return;
+        
+        var owner = enchantment.Card.Owner;
+        if (owner == null || owner.Creature.Side != side)
+            return;
+        
+        if (owner.PlayerCombatState.Energy <= 0)
+            return;
+
+        var card = enchantment.Card;
+        
+        if (card.Pile?.Type != PileType.Hand && card.Pile?.Type != PileType.Exhaust)
+            await CardPileCmd.Add(card, PileType.Hand);
+
+        card.SetToFreeThisTurn();
+        await CardCmd.AutoPlay(choiceContext, card, null, AutoPlayType.Default);
+    }
+}
