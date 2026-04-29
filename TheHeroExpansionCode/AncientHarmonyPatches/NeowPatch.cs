7703c54b4b6feb3fcd8f6283dcae65207551e0b3
BlackHero20 <guyshzu@gmail.com>
2026-04-29 08:35:48 +0300

    feat: added functionality to adding relic to existing ancient pools (excluding darv)

---
+++ TheHeroExpansionCode/AncientHarmonyPatches/NeowPatch.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using MegaCrit.Sts2.Core.Events;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Events;
+using TheHeroExpansion.TheHeroExpansionCode.Relics;
+
+namespace TheHeroExpansion.TheHeroExpansionCode;
+
+[HarmonyPatch(typeof(Neow), "get_PositiveOptions")]
+public static class NeowPatch
+{
+    public static void Postfix(Neow __instance, ref IEnumerable<EventOption> __result)
+    {
+        if (__instance.Owner == null)
+            return;
+
+        var relic = ModelDb.Relic<TestRelic>().ToMutable(); //change this to whatever relic I want
+        relic.Owner = __instance.Owner;
+
+        // Use reflection to call the protected RelicOption method
+        var method = typeof(AncientEventModel).GetMethod("RelicOption",
+            System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance,
+            null, new[] { typeof(RelicModel), typeof(string), typeof(string) }, null);
+
+        var option = (EventOption)method.Invoke(__instance, new object[] { relic, "INITIAL", null });
+        __result = __result.Append(option);
+    }
+}
