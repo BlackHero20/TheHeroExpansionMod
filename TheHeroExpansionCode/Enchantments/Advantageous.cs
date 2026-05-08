9bca8fc863d16df030d3072103f7b94aca907df3
BlackHero20 <guyshzu@gmail.com>
2026-05-08 11:21:57 +0300

    feat: added repurpose

---
+++ TheHeroExpansionCode/Enchantments/Advantageous.cs
@@ -0,0 +1,22 @@
+﻿using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Enchantments;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+
+namespace TheHeroExpansion.TheHeroExpansionCode.Enchantments;
+
+public class Advantageous : TheHeroExpansionEnchantment
+{
+    public override bool HasExtraCardText => true;
+    
+    public override bool ShowAmount => true;
+
+    public override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay? cardPlay)
+    {
+        Advantageous advantageous = this;
+        if (advantageous.Status != EnchantmentStatus.Normal)
+            return;
+        await PlayerCmd.GainEnergy((Decimal) advantageous.Amount, advantageous.Card.Owner);
+        advantageous.Status = EnchantmentStatus.Disabled;
+    }
+}
---
+++ TheHeroExpansionCode/Powers/RepurposePower.cs
@@ -0,0 +1,56 @@
+﻿using MegaCrit.Sts2.Core.CardSelection;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Hooks;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Powers;
+using TheHeroExpansion.TheHeroExpansionCode.Enchantments;
+
+namespace TheHeroExpansion.TheHeroExpansionCode.Powers;
+public sealed class RepurposePower : TheHeroExpansionPower
+{
+    public override PowerType Type => PowerType.Buff;
+    public override PowerStackType StackType => PowerStackType.Counter;
+
+    private decimal _advantageousAmount = 2M;
+
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new DynamicVar("Advantageous", _advantageousAmount)
+    ];
+
+    public void SetAdvantageous(decimal amount)
+    {
+        this.AssertMutable();
+        _advantageousAmount = amount;
+        this.DynamicVars["Advantageous"].BaseValue = amount;
+    }
+
+    public override async Task BeforeFlushLate(PlayerChoiceContext choiceContext, Player player)
+    {
+        RepurposePower power = this;
+        if (player != power.Owner.Player)
+            return;
+        if (!Hook.ShouldFlush(player.Creature.CombatState, player))
+            return;
+
+        CardSelectorPrefs prefs = new CardSelectorPrefs(power.SelectionScreenPrompt, 0, power.Amount);
+        List<CardModel> selected = (await CardSelectCmd.FromHand(
+            choiceContext, player, prefs,
+            card => card.Enchantment == null &&
+                    (card.Type == CardType.Attack || card.Type == CardType.Skill || card.Type == CardType.Power),
+            (AbstractModel)power)).ToList();
+
+        foreach (CardModel card in selected)
+        {
+            card.GiveSingleTurnRetain();
+            CardCmd.Enchant<Advantageous>(card, _advantageousAmount);
+        }
+
+        await PowerCmd.Remove(power);
+    }
+}
