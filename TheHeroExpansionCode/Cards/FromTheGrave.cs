2fddb59eb2b33961199e5563105d922d473431b1
BlackHero20 <guyshzu@gmail.com>
2026-05-09 00:59:04 +0300

    feat: added from the grave

---
+++ TheHeroExpansionCode/Cards/FromTheGrave.cs
@@ -0,0 +1,43 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace TheHeroExpansion.TheHeroExpansionCode.Cards;
+[Pool(typeof(IroncladCardPool))]
+public class FromTheGrave() : TheHeroExpansionCard(2,
+    CardType.Skill, CardRarity.Uncommon,
+    TargetType.Self)
+{
+    public override bool GainsBlock => true;
+
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new BlockVar(14M, ValueProp.Move)
+    ];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay play)
+    {
+        FromTheGrave card = this;
+        await CreatureCmd.GainBlock(card.Owner.Creature, card.DynamicVars.Block, play);
+    }
+
+    public override async Task AfterCardExhausted(
+        PlayerChoiceContext choiceContext,
+        CardModel card,
+        bool causedByEthereal)
+    {
+        FromTheGrave fromTheGrave = this;
+        if (card != fromTheGrave) return;
+        
+        await CreatureCmd.GainBlock(fromTheGrave.Owner.Creature, fromTheGrave.DynamicVars.Block, null);
+        CardCmd.PreviewCardPileAdd(await CardPileCmd.AddGeneratedCardToCombat(
+            fromTheGrave.CreateClone(), PileType.Draw, fromTheGrave.Owner, CardPilePosition.Random));
+    }
+
+    protected override void OnUpgrade() => this.DynamicVars.Block.UpgradeValueBy(4M);
+}
