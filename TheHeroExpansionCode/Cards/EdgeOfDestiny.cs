0f8a673759ee9318f02e73dabc0cf89ab7d4edd2
BlackHero20 <guyshzu@gmail.com>
2026-05-10 22:54:49 +0300

    feat: added edge of destiny

---
+++ TheHeroExpansionCode/Cards/EdgeOfDestiny.cs
@@ -0,0 +1,36 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Models.Cards;
+using TheHeroExpansion.TheHeroExpansionCode.Cards;
+using TheHeroExpansion.TheHeroExpansionCode.Powers;
+
+namespace TheHeroExpansion.TheHeroExpansionCode.Cards;
+
+[Pool(typeof(RegentCardPool))]
+public class EdgeOfDestiny() : TheHeroExpansionCard(1,
+    CardType.Power, CardRarity.Rare, TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new PowerVar<EdgeOfDestinyPower>(3M)
+    ];
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips =>
+    [
+        HoverTipFactory.FromCard<SovereignBlade>()
+    ];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay play)
+    {
+        EdgeOfDestiny card = this;
+        await PowerCmd.Apply<EdgeOfDestinyPower>(choiceContext, card.Owner.Creature,
+            card.DynamicVars["EdgeOfDestinyPower"].BaseValue, card.Owner.Creature, card);
+    }
+
+    protected override void OnUpgrade() => this.DynamicVars["EdgeOfDestinyPower"].UpgradeValueBy(2M);
+}
---
+++ TheHeroExpansionCode/Patches/SovereignBladePatch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Cards;
+using MegaCrit.Sts2.Core.ValueProps;
+using TheHeroExpansion.TheHeroExpansionCode.Powers;
+
+namespace TheHeroExpansion.TheHeroExpansionCode.Patches;
+
+[HarmonyPatch(typeof(SovereignBlade))]
+[HarmonyPatch("CanonicalVars", MethodType.Getter)]
+public static class SovereignBladePatch
+{
+    public static void Postfix(ref IEnumerable<DynamicVar> __result)
+    {
+        __result = __result.Append(new DamageVar("EdgeOfDestinyDamage", 0M, ValueProp.Move));
+    }
+}
