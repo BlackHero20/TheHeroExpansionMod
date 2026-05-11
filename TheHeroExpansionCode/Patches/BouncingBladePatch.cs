cd564451e2a9515752391abec73e637c966d29af
BlackHero20 <guyshzu@gmail.com>
2026-05-11 23:13:30 +0300

    feat: added bouncing blade

---
+++ TheHeroExpansionCode/Patches/BouncingBladePatch.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using MegaCrit.Sts2.Core.Nodes.Cards;
+using MegaCrit.Sts2.Core.Models;
+using TheHeroExpansion.TheHeroExpansionCode.Cards;
+
+namespace TheHeroExpansion.TheHeroExpansionCode.Patches;
+
+[HarmonyPatch(typeof(NCard), nameof(NCard.AnimMultiCardPlay))]
+public static class BouncingBladePatch
+{
+    public static bool Prefix(NCard __instance, ref Task __result)
+    {
+        if (__instance.Model is BouncingBlade)
+        {
+            __result = Task.CompletedTask;
+            return false;
+        }
+        return true;
+    }
+}
