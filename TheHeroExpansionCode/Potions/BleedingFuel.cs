7d110c308c0fcf1e8e1d9e47080ef93d3a147c8c
BlackHero20 <guyshzu@gmail.com>
2026-05-30 15:35:32 +0300

    feat: added 3 new potions. VIgor Potion, Time in a Bottle, and Bleeding Fuel

---
+++ TheHeroExpansionCode/Potions/BleedingFuel.cs
@@ -0,0 +1,29 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.CardSelection;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Potions;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.PotionPools;
+
+namespace TheHeroExpansion.TheHeroExpansionCode.Potions;
+[Pool(typeof(SharedPotionPool))]
+public class BleedingFuel : TheHeroExpansionPotion
+{
+    public override PotionRarity Rarity => PotionRarity.Rare;
+
+    public override PotionUsage Usage => PotionUsage.CombatOnly;
+
+    public override TargetType TargetType => TargetType.Self;
+
+    protected override async Task OnUse(PlayerChoiceContext choiceContext, Creature? target)
+    {
+        BleedingFuel bleedingFuel = this;
+        CardModel card = (await CardSelectCmd.FromCombatPile(choiceContext, PileType.Exhaust.GetPile(bleedingFuel.Owner), bleedingFuel.Owner, new CardSelectorPrefs(bleedingFuel.SelectionScreenPrompt, 1))).FirstOrDefault<CardModel>();
+        if (card == null)
+            return;
+        await CardPileCmd.Add(card, PileType.Hand);
+    }
+}
---
+++ TheHeroExpansionCode/Powers/MomentumPower.cs
@@ -0,0 +1,38 @@
+﻿using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace TheHeroExpansion.TheHeroExpansionCode.Powers;
+public class MomentumPower : TheHeroExpansionPower
+{
+    public override PowerType Type => PowerType.Buff;
+    public override PowerStackType StackType => PowerStackType.Counter;
+    
+    public override decimal ModifyDamageAdditive(Creature? target, decimal amount, ValueProp props, Creature? dealer, CardModel? cardSource)
+    {
+        return Owner != dealer || !props.IsPoweredAttack() ? 0M : (decimal)Amount;
+    }
+    
+    public override decimal ModifyBlockAdditive(Creature target, decimal block, ValueProp props, CardModel? cardSource, CardPlay? cardPlay)
+    {
+        if (cardSource != null)
+        {
+            if (cardSource.Owner.Creature != Owner)
+                return 0M;
+        }
+        else if (Owner != target)
+            return 0M;
+        return !props.IsPoweredCardOrMonsterMoveBlock() ? 0M : (decimal)Amount;
+    }
+    
+    public override async Task AfterCardPlayed(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        if (cardPlay.Card.Owner?.Creature != Owner)
+            return;
+        await PowerCmd.Apply<MomentumPower>(choiceContext, Owner, -1M, Owner, null);
+    }
+}
