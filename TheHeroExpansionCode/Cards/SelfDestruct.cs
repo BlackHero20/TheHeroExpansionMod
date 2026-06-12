4847e608695a29937f1eb83fd8543610377a8ef7
BlackHero20 <guyshzu@gmail.com>
2026-06-12 19:43:30 +0300

    feat: added Self Destruct Card

---
+++ TheHeroExpansionCode/Cards/SelfDestruct.cs
@@ -0,0 +1,55 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Extensions;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using System.Linq;
+using TheHeroExpansion.TheHeroExpansionCode.Cards;
+
+namespace TheHeroExpansion.TheHeroExpansionCode.Cards;
+
+[Pool(typeof(DefectCardPool))]
+public class SelfDestruct() : TheHeroExpansionCard(3,
+    CardType.Skill, CardRarity.Rare,
+    TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new DynamicVar("Attacks", 3M),
+        new DynamicVar("OrbSlots", 3M)
+    ];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        SelfDestruct selfDestruct = this;
+        int attackCount = DynamicVars["Attacks"].IntValue;
+
+        for (int i = 0; i < attackCount; i++)
+        {
+            var attack = PileType.Draw.GetPile(Owner).Cards
+                             .Where(c => c.Type == CardType.Attack && !c.Keywords.Contains(CardKeyword.Unplayable))
+                             .ToList()
+                             .StableShuffle(Owner.RunState.Rng.Shuffle)
+                             .FirstOrDefault()
+                         ?? PileType.Draw.GetPile(Owner).Cards
+                             .Where(c => c.Type == CardType.Attack)
+                             .ToList()
+                             .StableShuffle(Owner.RunState.Rng.Shuffle)
+                             .FirstOrDefault();
+
+            if (attack == null) break;
+            await CardCmd.AutoPlay(choiceContext, attack, null);
+        }
+        await CreatureCmd.TriggerAnim(selfDestruct.Owner.Creature, "PowerUp", selfDestruct.Owner.Character.PowerUpAnimDelay);
+        OrbCmd.RemoveSlots(Owner, DynamicVars["OrbSlots"].IntValue);
+    }
+
+    protected override void OnUpgrade()
+    {
+        this.DynamicVars["Attacks"].UpgradeValueBy(1M);
+    }
+}
