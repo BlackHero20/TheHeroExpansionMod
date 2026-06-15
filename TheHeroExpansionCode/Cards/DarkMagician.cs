620a61590283a4729f7c107be52841b8203c2205
BlackHero20 <guyshzu@gmail.com>
2026-06-15 22:03:14 +0300

    feat: added Dark Magician

---
+++ TheHeroExpansionCode/Cards/DarkMagician.cs
@@ -0,0 +1,53 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Models.Cards;
+using MegaCrit.Sts2.Core.Models.Orbs;
+using MegaCrit.Sts2.Core.Models.Powers;
+using TheHeroExpansion.TheHeroExpansionCode.Cards;
+using TheHeroExpansion.TheHeroExpansionCode.Powers;
+using Void = MegaCrit.Sts2.Core.Models.Cards.Void;
+
+namespace TheHeroExpansion.TheHeroExpansionCode.Cards;
+
+[Pool(typeof(DefectCardPool))]
+public class DarkMagician() : TheHeroExpansionCard(1,
+    CardType.Skill, CardRarity.Uncommon,
+    TargetType.Self)
+{
+    public override IEnumerable<CardKeyword> CanonicalKeywords => 
+    [CardKeyword.Exhaust];
+
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new PowerVar<DarkMagicianPower>(1M)
+    ];
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips =>
+    [
+        HoverTipFactory.Static(StaticHoverTip.Channeling),
+        HoverTipFactory.FromOrb<LightningOrb>(),
+        HoverTipFactory.FromOrb<DarkOrb>(),
+        HoverTipFactory.FromCard<Void>()
+    ];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        DarkMagician darkMagician = this;
+        await PowerCmd.Apply<DarkMagicianPower>(choiceContext, darkMagician.Owner.Creature, darkMagician.DynamicVars["DarkMagicianPower"].BaseValue, darkMagician.Owner.Creature, darkMagician);
+        CardCmd.PreviewCardPileAdd(await CardPileCmd.AddGeneratedCardToCombat((CardModel) darkMagician.CombatState.CreateCard<Void>(darkMagician.Owner), PileType.Discard, darkMagician.Owner));
+        await Cmd.Wait(0.5f);
+    }
+
+    protected override void OnUpgrade()
+    {
+        this.EnergyCost.UpgradeBy(-1);
+    }
+}
---
+++ TheHeroExpansionCode/Patches/DarkMagicianPatch.cs
@@ -0,0 +1,31 @@
+﻿using HarmonyLib;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Orbs;
+using TheHeroExpansion.TheHeroExpansionCode.Powers;
+
+namespace TheHeroExpansion.TheHeroExpansionCode.Patches;
+
+public static class DarkMagicianPatch
+{
+    [HarmonyPatch(typeof(OrbCmd), nameof(OrbCmd.Channel),
+        new[] { typeof(PlayerChoiceContext), typeof(OrbModel), typeof(Player) })]
+    public static class OrbChannelPatch
+    {
+        public static void Postfix(
+            ref Task __result,
+            PlayerChoiceContext choiceContext,
+            OrbModel orb,
+            Player player)
+        {
+            if (player.Creature.GetPower<DarkMagicianPower>() == null) return;
+            if (orb is not LightningOrb) return;
+
+            __result = __result
+                .ContinueWith(_ => OrbCmd.Channel<DarkOrb>(choiceContext, player))
+                .Unwrap();
+        }
+    }
+}
