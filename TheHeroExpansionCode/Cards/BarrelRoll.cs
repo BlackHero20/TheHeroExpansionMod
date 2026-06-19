441141527a4b9d73a5d597eecadc5b07eadaef27
BlackHero20 <guyshzu@gmail.com>
2026-06-19 22:25:22 +0300

    feat: added Barrel Roll

---
+++ TheHeroExpansionCode/Cards/BarrelRoll.cs
@@ -0,0 +1,49 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Models.Cards;
+using MegaCrit.Sts2.Core.ValueProps;
+using TheHeroExpansion.TheHeroExpansionCode.Cards;
+
+namespace TheHeroExpansion.TheHeroExpansionCode.Cards;
+
+[Pool(typeof(RegentCardPool))]
+public class BarrelRoll() : TheHeroExpansionCard(1,
+    CardType.Skill, CardRarity.Common,
+    TargetType.Self)
+{
+    public override bool GainsBlock => true;
+
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new BlockVar(11M, ValueProp.Move)
+    ];
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips =>
+    [
+        HoverTipFactory.FromCard<Debris>()
+    ];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        BarrelRoll card = this;
+        await CreatureCmd.GainBlock(card.Owner.Creature, card.DynamicVars.Block, cardPlay);
+
+        var debrisPile = card.IsUpgraded ? PileType.Discard : PileType.Draw;
+        CardCmd.PreviewCardPileAdd(
+            await CardPileCmd.AddGeneratedCardToCombat(
+                card.CombatState.CreateCard<Debris>(card.Owner),
+                debrisPile, card.Owner));
+
+        await Cmd.Wait(0.5f);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Block.UpgradeValueBy(2M);
+    }
+}
