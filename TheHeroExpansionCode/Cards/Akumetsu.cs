07949e917370536316712cf5c5e0b1109b99989e
BlackHero20 <guyshzu@gmail.com>
2026-06-19 21:23:42 +0300

    feat: added akumetsu

---
+++ TheHeroExpansionCode/Cards/Akumetsu.cs
@@ -0,0 +1,44 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
+using TheHeroExpansion.TheHeroExpansionCode.Cards;
+using TheHeroExpansion.TheHeroExpansionCode.Powers;
+
+namespace TheHeroExpansion.TheHeroExpansionCode.Cards;
+[Pool(typeof(IroncladCardPool))]
+public class Akumetsu() : TheHeroExpansionCard(2,
+    CardType.Skill, CardRarity.Uncommon,
+    TargetType.Self)
+{
+    public override bool GainsBlock => true;
+
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new BlockVar(12M, ValueProp.Move)
+    ];
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips =>
+    [
+        HoverTipFactory.Static(StaticHoverTip.Block)
+    ];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        Akumetsu card = this;
+        await CreatureCmd.GainBlock(card.Owner.Creature, card.DynamicVars.Block, cardPlay);
+        await PowerCmd.Apply<AkumetsuPower>(choiceContext, card.Owner.Creature,
+            1M, card.Owner.Creature, card);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Block.UpgradeValueBy(4M);
+    }
+}
---
+++ TheHeroExpansionCode/Powers/AstralPower.cs
@@ -0,0 +1,38 @@
+﻿using BaseLib.Abstracts;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+using TheHeroExpansion.TheHeroExpansionCode.Extensions;
+
+namespace TheHeroExpansion.TheHeroExpansionCode.Powers;
+public sealed class AstralPower : TheHeroExpansionPower
+{
+    public override PowerType Type => PowerType.Buff;
+    public override PowerStackType StackType => PowerStackType.Counter;
+
+    public override decimal ModifyDamageMultiplicative(
+        Creature? target,
+        decimal amount,
+        ValueProp props,
+        Creature? dealer,
+        CardModel? cardSource)
+    {
+        if (dealer != Owner && !Owner.Pets.Contains(dealer)) return 1M;
+        if (!props.IsPoweredAttack()) return 1M;
+        if (cardSource == null) return 1M;
+        return 2M;
+    }
+
+    public override async Task AfterCardPlayed(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        AstralPower power = this;
+        if (cardPlay.Card.Owner != power.Owner.Player) return;
+        if (cardPlay.Card.Type != CardType.Attack) return;
+
+        await PowerCmd.Decrement(power);
+    }
+}
