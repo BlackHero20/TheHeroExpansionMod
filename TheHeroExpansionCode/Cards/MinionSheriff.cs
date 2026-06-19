f6522b948acea573cb83d1bcf88e6b69014f7f28
BlackHero20 <guyshzu@gmail.com>
2026-06-19 22:25:33 +0300

    feat: Added FIRE!!!

---
+++ TheHeroExpansionCode/Cards/MinionSheriff.cs
@@ -0,0 +1,44 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Nodes.CommonUi;
+using System.Linq;
+using TheHeroExpansion.TheHeroExpansionCode.Cards;
+
+namespace TheHeroExpansion.TheHeroExpansionCode.Cards;
+[Pool(typeof(TokenCardPool))]
+public class MinionSheriff() : TheHeroExpansionCard(1,
+    CardType.Skill, CardRarity.Token,
+    TargetType.Self)
+{
+    public override IEnumerable<CardKeyword> CanonicalKeywords =>
+        [CardKeyword.Exhaust];
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => [];
+
+    protected override HashSet<CardTag> CanonicalTags =>
+        [CardTag.Minion];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        MinionSheriff card = this;
+
+        var minionCards = PileType.Exhaust.GetPile(card.Owner).Cards
+            .Where(c => c.Tags.Contains(CardTag.Minion) && c != card && c is not MinionSheriff)
+            .ToList();
+
+        bool first = true;
+        foreach (var minion in minionCards)
+        {
+            if (card.IsUpgraded && minion.IsUpgradable)
+                CardCmd.Upgrade(minion, CardPreviewStyle.None);
+
+            await CardCmd.AutoPlay(choiceContext, minion, null, skipCardPileVisuals: false);
+        }
+    }
+
+    protected override void OnUpgrade() { }
+}
