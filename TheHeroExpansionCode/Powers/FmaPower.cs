fa1579f1bffe9d200a48af85c55f5cf8e0d01261
BlackHero20 <guyshzu@gmail.com>
2026-06-20 12:18:48 +0300

    feat: added f=ma

---
+++ TheHeroExpansionCode/Powers/FmaPower.cs
@@ -0,0 +1,28 @@
+﻿using BaseLib.Abstracts;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
+using TheHeroExpansion.TheHeroExpansionCode.Extensions;
+
+namespace TheHeroExpansion.TheHeroExpansionCode.Powers;
+public sealed class FmaPower : TheHeroExpansionPower
+{
+    public override PowerType Type => PowerType.Buff;
+    public override PowerStackType StackType => PowerStackType.Single;
+
+    public override decimal ModifyDamageAdditive(
+        Creature? target,
+        decimal amount,
+        ValueProp props,
+        Creature? dealer,
+        CardModel? cardSource)
+    {
+        if (Owner != dealer) return 0M;
+        if (!props.IsPoweredAttack()) return 0M;
+
+        var dex = Owner.GetPower<DexterityPower>();
+        return dex != null ? (decimal) dex.Amount : 0M;
+    }
+}
